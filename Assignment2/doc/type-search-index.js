9c9540e5d6bc90b7bcb2d5ba3632a60fe0337091
MBHNKU001-MSBAKH003 <88949829+MBHNKU001-MSBAKH003@users.noreply.github.com>
2021-08-15 13:14:52 +0200

    Delete Assignment2 directory

--- Assignment2/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"<Unnamed>","l":"AccessAVLApp"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"AVLStudent"},{"p":"<Unnamed>","l":"ExperimentAVLApp"}]
